8485b0fae36045f8e7ee2c6abcf95f307feb4bb8
张伟 <zhangwei19890518@gmail.com>
2017-03-19 15:56:12 +0800

    complete the homework

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,17 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, :only => [:new, :create]
+  before_action :authenticate_user!, :only => [:new, :create, :update, :destroy]
 
   def new
     @group = Group.find(params[:group_id])
     @post = Post.new
+  end
+
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    if current_user != @post.user
+      redirect_to account_posts_path, alert: "你没有权限编辑文章！"
+    end
   end
 
   def create
@@ -16,6 +24,27 @@ class PostsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def update
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    if current_user != @post.user
+      redirect_to account_posts_path, alert: "你没有权限修改文章！"
+    end
+    if @post.update(post_params)
+      redirect_to account_posts_path, alert: "文章更新成功！"
+    end
+  end
+
+  def destroy
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    if current_user != @post.user
+      redirect_to account_posts_path, alert: "你没有权限删除文章！"
+    end
+    @post.destroy
+    redirect_to account_posts_path, alert: "删除文章成功！"
   end
 
   private
